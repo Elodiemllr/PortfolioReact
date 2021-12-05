3963e69e07dd7cb34bd2fe0df2350370687af308
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-05 18:56:31 +0100

    modification avec iconify

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,68 +1,85 @@
-import AppsIcon from "@mui/icons-material/Apps";
-import AutoGraphIcon from "@mui/icons-material/AutoGraph";
-import BallotIcon from "@mui/icons-material/Ballot";
-import Brightness6Icon from "@mui/icons-material/Brightness6";
-import BrushIcon from "@mui/icons-material/Brush";
-import CloseIcon from "@mui/icons-material/Close";
-import HomeIcon from "@mui/icons-material/Home";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import SendIcon from "@mui/icons-material/Send";
+import { Icon } from "@iconify/react";
 import React from "react";
 import "../styles/nav.scss";
 const Nav = () => {
     return (
-        <header>
-            <nav className="header">
+        <header className="header" id="header">
+            <nav className="nav container">
                 <a href="#" className="nav__logo">
-                    <img className="logo" alt="logo" src="./img/logo.svg"></img>
+                    <img
+                        className="nav__logo"
+                        alt="logo"
+                        src="./img/logo.svg"
+                    ></img>
                 </a>
                 <div className="nav__menu">
                     <ul className="nav__list grid">
                         <li className="nav__item">
                             <a href="#home" className="nav__link active-link">
-                                <HomeIcon className="nav__icon" /> Home
+                                <Icon
+                                    icon="akar-icons:home"
+                                    className="nav__icon"
+                                />{" "}
+                                Home
                             </a>
                         </li>
                         <li className="nav__item">
                             <a href="#about" className="nav__link active-link">
-                                <PermIdentityIcon className="nav__icon" /> About
-                                me
+                                <Icon icon="uil:user" className="nav__icon" />
+                                About me
                             </a>
                         </li>
                         <li className="nav__item">
                             <a href="#skills" className="nav__link active-link">
-                                <BrushIcon className="nav__icon" />
+                                <Icon
+                                    icon="uil:file-info-alt"
+                                    className="nav__icon"
+                                />
                                 Skills
                             </a>
                         </li>
                         <li className="nav__item">
                             <a href="#home" className="nav__link active-link">
-                                <AutoGraphIcon className="nav__icon" />
+                                <Icon
+                                    icon="uil:analysis"
+                                    className="nav__icon"
+                                />
                                 Qualification
                             </a>
                         </li>
                         <li className="nav_item">
                             <a href="#portfolio" className="nav__link">
-                                <BallotIcon className="nav__icon" />
+                                <Icon
+                                    icon="uil:files-landscapes"
+                                    className="nav__icon"
+                                />
                                 Portfolio
                             </a>
                         </li>
                         <li className="nav_item">
                             <a href="#contact" className="nav__link">
-                                <SendIcon className="nav__icon" />
+                                <Icon
+                                    icon="uil:message"
+                                    className="nav__icon"
+                                />
                                 Contact
                             </a>
                         </li>
                     </ul>
-                    <CloseIcon className="nav__close" id="nav-close" />
+                    <Icon
+                        icon="ant-design:close-outlined"
+                        className="nav__close"
+                        id="nav-close"
+                    />
                 </div>
                 <div className="nav__btns">
-                    <Brightness6Icon
+                    <Icon
+                        icon="gg:dark-mode"
                         className="nav__changetheme"
                         id="theme-button"
                     />
                     <div class="nav__toggle" id="nav-toggle">
-                        <AppsIcon />
+                        <Icon icon="uil:apps" />
                     </div>
                 </div>
             </nav>
