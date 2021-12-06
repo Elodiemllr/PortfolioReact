3f348c8bee1378524cc89e93fd5b9d90936e94dc
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-06 16:38:05 +0100

    creation of qualification comp

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About.js";
 import Home from "./components/Home.js";
 import Nav from "./components/Nav.js";
+import Qualification from "./components/Qualification.js";
 import Skills from "./components/Skills.js";
 import "./styles/App.scss";
 
@@ -20,6 +21,7 @@ function App() {
                                     <Home />
                                     <About />
                                     <Skills />
+                                    <Qualification />
                                 </main>
                             </>
                         }
---
+++ client/src/components/Qualification.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Qualification.scss";
+
+const Qualification = () => {
+    return <h2> coucou</h2>;
+};
+
+export default Qualification;
