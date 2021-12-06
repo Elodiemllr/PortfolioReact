28dcac539dc7cd6e96e3d6bd20f7462ea029ffc3
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-06 16:15:24 +0100

    skill comp and scss

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About.js";
 import Home from "./components/Home.js";
 import Nav from "./components/Nav.js";
+import Skills from "./components/Skills.js";
 import "./styles/App.scss";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
                                 <main className="main">
                                     <Home />
                                     <About />
+                                    <Skills />
                                 </main>
                             </>
                         }
---
+++ client/src/components/Skills.js
@@ -0,0 +1,170 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+import "../styles/Skills.scss";
+
+const Skills = () => {
+    return (
+        <section className="skills section" id="skills">
+            <h2 className="section__title">Skills</h2>
+            <span className="section__subtitle">My technical level </span>
+            <div className="skills__container container grid">
+                <div className="skills__contain">
+                    <div className="skills__content skills__close">
+                        <div className="skills__header">
+                            <Icon icon="uil:apps" className="skills__icon" />
+                            <div>
+                                <h1 className="skills__title">
+                                    Frontend developer
+                                </h1>
+                                <span className="skills__subtitle">
+                                    O'clock formation
+                                </span>
+                            </div>
+                            <Icon
+                                icon="uil:angle-up"
+                                className="skills__arrow"
+                            />
+                        </div>
+
+                        <div className="skills__list grid">
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">HTML</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">CSS</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">javaScript</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">React</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="skills__content skills__close">
+                        <div className="skills__header">
+                            <Icon
+                                icon="uil:database-alt"
+                                className="skills__icon"
+                            />
+                            <div>
+                                <h1 className="skills__title">
+                                    Backend developer
+                                </h1>
+                                <span className="skills__subtitle">
+                                    O'clock formation
+                                </span>
+                            </div>
+                            <Icon
+                                icon="uil:angle-up"
+                                className="skills__arrow"
+                            />
+                        </div>
+
+                        <div className="skills__list grid">
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">NODE js</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">PostgreSQL</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">mongoDB</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Express</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Postman</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="skills__content skills__close">
+                        <div className="skills__header">
+                            <Icon icon="uil:screw" className="skills__icon" />
+                            <div>
+                                <h1 className="skills__title">TOOLS</h1>
+                                <span className="skills__subtitle">
+                                    O'clock formation
+                                </span>
+                            </div>
+                            <Icon
+                                icon="uil:angle-up"
+                                className="skills__arrow"
+                            />
+                        </div>
+
+                        <div className="skills__list grid">
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">VS code</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">GIT</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Github</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Trello</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Whimsical</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Figma</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Canva</h3>
+                                </div>
+                            </div>
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Gimp</h3>
+                                </div>
+                            </div>{" "}
+                            <div className="skills__data">
+                                <div className="skills__titles">
+                                    <h3 className="skills__name">Da Vinci</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Skills;
