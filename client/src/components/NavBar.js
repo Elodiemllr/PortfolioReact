33c7cd51b66a48698fbd002fb27bd15dc5ac5fe7
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-15 16:51:27 +0100

    creation of showMenu function

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,42 +1,82 @@
+import { Icon } from "@iconify/react";
 import React, { useState } from "react";
 import "../styles/NavBar.scss";
 
 const NavBar = () => {
     const [moveMenu, setMoveMenu] = useState(false);
-
+    const [showNav, setShowNav] = useState(false);
     const showMoveMenu = () => {
         setMoveMenu(!moveMenu);
     };
 
     const showMenu = () => {
-        console.log("hey");
+        setShowNav(!showNav);
     };
 
     return (
-        <div className={` ${moveMenu ? "navBarShow " : "navBar"}`}>
-            <div
-                className="navBar__content"
-                onMouseEnter={showMoveMenu}
-                onMouseLeave={showMoveMenu}
-                onClick={showMenu}
-            >
-                <div className="navBar__lines">
-                    <div className="navBar__line"></div>
+        <div
+            className={` ${moveMenu ? "navBarShow " : "navBar"} ${
+                showNav ? "showNav" : ""
+            }`}
+        >
+            {!showNav ? (
+                <div
+                    className="navBar__content"
+                    onMouseEnter={showMoveMenu}
+                    onMouseLeave={showMoveMenu}
+                    onClick={showMenu}
+                >
+                    <div className="navBar__lines">
+                        <div className="navBar__line"></div>
+                        <div
+                            className={` ${
+                                moveMenu ? "navBar__center" : "navBar__line"
+                            }`}
+                        ></div>
+                        <div className="navBar__line"></div>
+                    </div>
                     <div
                         className={` ${
-                            moveMenu ? "navBar__center" : "navBar__line"
+                            moveMenu ? "  navBar__titleShow" : "navBar__title"
                         }`}
-                    ></div>
-                    <div className="navBar__line"></div>
+                    >
+                        <p> Menu </p>
+                    </div>
                 </div>
-                <div
-                    className={` ${
-                        moveMenu ? "  navBar__titleShow" : "navBar__title"
-                    }`}
-                >
-                    <p> Menu </p>
+            ) : (
+                <div>
+                    <Icon
+                        icon="ant-design:close-outlined"
+                        className="nav__close"
+                        id="nav-close"
+                    />
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                About
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                Skills
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                Qualification
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                Work
+                                <a href="#"> Work 1</a>
+                                <a href="#"> Work 2</a>
+                                <a href="#"> Work 3</a>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
