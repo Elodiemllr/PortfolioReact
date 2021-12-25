c69eddd5be65311bc75c72e739f518f5a969a454
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-12-25 23:41:32 +0100

    modification homme css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,33 +6,17 @@ const Home = () => {
     return (
         <section className="home section" id="home">
             <div className="home__container container grid">
-                <div className="home__scroll">
-                    <video
-                        width="auto"
-                        height="auto"
-                        autoPlay
-                        loop="true"
-                        muted
-                    >
-                        <source src={videoElodie} type="video/mp4" />
-                    </video>
-                    <a
-                        href="#about"
-                        className="home__scroll-button button--flex"
-                    >
-                        <Icon
-                            icon="uil:mouse-alt-2"
-                            className="home__scroll-mouse"
-                        />
-
-                        <span className="home__scroll-name">Scroll down</span>
-
-                        <Icon
-                            icon="uil:angle-down"
-                            className="home__scroll-arrow"
-                        />
-                    </a>
-                </div>
+                <div className="home__spacer"> </div>
+                <video width="auto" height="auto" autoPlay loop="true" muted>
+                    <source src={videoElodie} type="video/mp4" />
+                </video>
+                <div className="home__spacer"> </div>
+                <a href="#about" className="home__scroll__button button--flex">
+                    <Icon
+                        icon="bi:arrow-down"
+                        className="home__scroll__arrow"
+                    />
+                </a>
             </div>
         </section>
     );
