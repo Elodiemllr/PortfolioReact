dda0cb2235bb4dbcce1205facd935cc779073203
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2022-01-10 12:12:05 +0100

    responsive about min451px done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -34,40 +34,41 @@ const About = () => {
                 <div className="about__img">
                     <img src="./img/elodie.jpg" alt="elodie" />
                 </div>
+                <div className="about__infos">
+                    <div className="about__content">
+                        <div className="about__sub">
+                            <span className="about__subtitle">
+                                Moi c'est Elodie, une jeune développeuse située
+                                au Luxembourg, passionnée de communication,
+                                programmation et design web.
+                            </span>
+                        </div>
 
-                <div className="about__content">
-                    <div className="about__sub">
-                        <span className="about__subtitle">
-                            Moi c'est Elodie, une jeune développeuse située au
-                            Luxembourg, passionnée de communication,
-                            programmation et design web.
-                        </span>
+                        <p className="about__description">
+                            J'ai commencé mon parcours en tant que commerciale,
+                            mais ma passion pour le design web et l'informatique
+                            m'a poussé à reprendre des études. J'ai donc voulu
+                            découvrir le monde de l'informatique et j'ai ainsi
+                            entrepris une formation de six mois afin d'obtenir
+                            un diplôme de développeur web et web mobile. ors de
+                            cette formation, j'ai pu acquérir de solides
+                            connaissances informatiques en réalisant diverses
+                            applications web et web mobile. Cela m'a permis
+                            d'apprendre à maitriser plusieurs langages comme
+                            html, css et javascript (dont node et react).
+                        </p>
+
+                        <br />
                     </div>
-
-                    <p className="about__description">
-                        J'ai commencé mon parcours en tant que commerciale, mais
-                        ma passion pour le design web et l'informatique m'a
-                        poussé à reprendre des études. J'ai donc voulu découvrir
-                        le monde de l'informatique et j'ai ainsi entrepris une
-                        formation de six mois afin d'obtenir un diplôme de
-                        développeur web et web mobile. ors de cette formation,
-                        j'ai pu acquérir de solides connaissances informatiques
-                        en réalisant diverses applications web et web mobile.
-                        Cela m'a permis d'apprendre à maitriser plusieurs
-                        langages comme html, css et javascript (dont node et
-                        react).
-                    </p>
-                    <div className="about__info"></div>
-                    <br />
-                </div>
-                <div className="about__buttons">
-                    <a
-                        download=""
-                        href="https://www.canva.com/design/DAExxQ715Qo/dYY_hdzcU7QxezoOi_vf4Q/view?utm_content=DAExxQ715Qo&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
-                        className="button button--flex"
-                    >
-                        Télecharge mon CV ici
-                    </a>
+                    <div className="about__buttons">
+                        <a
+                            download=""
+                            href="https://www.canva.com/design/DAExxQ715Qo/dYY_hdzcU7QxezoOi_vf4Q/view?utm_content=DAExxQ715Qo&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
+                            className="button button--flex"
+                        >
+                            Télecharge mon CV ici
+                        </a>
+                    </div>
                 </div>
             </section>
         </div>
